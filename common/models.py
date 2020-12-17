fa84626e269aab5a27e3d345440ed9f7cba3cc98
Benjamin Zagorsky <iamzags@gmail.com>
2020-12-17 13:22:46 -0500

    adds update method

--- common/models.py
+++ common/models.py
@@ -11,6 +11,16 @@ class TimestampedModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
+    def update(self, update_dict=None, **kwargs):
+        """ Helper method to update objects """
+        if not update_dict:
+            update_dict = kwargs
+        update_fields = {"updated_on"}
+        for k, v in update_dict.iteritems():
+            setattr(self, k, v)
+            update_fields.add(k)
+        self.save(update_fields=update_fields)
+    
     class Meta:
         abstract = True
 
