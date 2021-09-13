964b6e1cc7581a9dfe336572850af7e43a0466e8
kyfantaz <kyfantaz@users.noreply.github.com>
2021-09-12 23:09:19 -0400

    replaces custom ChoicesEnum with built-in TextChoices for Django 3.2.* projects

--- common/constants.py
+++ common/constants.py
@@ -1,14 +1,1 @@
-class ChoicesEnum(object):
-    @classmethod
-    def values(cls):
-        return [value for key, value in vars(cls).items() if not key.startswith("_")]
-
-    @classmethod
-    def choices(cls):
-        return [(value, value.replace("_", " ").title()) for value in cls.values()]
-
-
-class ExampleEnum(ChoicesEnum):
-    # TODO: delete me; this is just a reference example
-    active = "active"
-    completed = "completed"
+from django.db.models import TextChoices
