58e53b09e9816a088d26b2b5b3af43f0c76db1c8
Alvin Siu <alvin@zagaran.com>
2022-01-25 09:38:58 -0500

    Update maintainers instructions

--- .platform/hooks/predeploy/predeploy.sh
+++ .platform/hooks/predeploy/predeploy.sh
@@ -1,6 +1,5 @@
 # START_FEATURE elastic_beanstalk
 #!/bin/bash
-/opt/elasticbeanstalk/bin/get-config environment | jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' > /etc/profile.d/sh.local
 source $PYTHONPATH/activate
 
 # START_FEATURE django_react
